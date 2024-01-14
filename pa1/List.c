de40ad46d525a00e121114df830f7b3997418633
Sebastian Avila <snavila@ucsc.edu>
2024-01-13 20:11:41 -0800

    progress

--- pa1/List.c
+++ pa1/List.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <stdool.h>
+#include <stdbool.h>
 #include "List.h"
 
 
@@ -16,7 +16,7 @@
 typedef struct NodeObj* Node;
 
 // private NodeObj type
-typedef struct Nodeobj {
+typedef struct NodeObj {
 	ListElement data;
 	Node previous;
 	Node next;
@@ -29,7 +29,7 @@ typedef struct ListObj {
 	Node cursor;
 	int cursorIndex;
 	int length;
-} Listobj;
+} ListObj;
 
 
 
@@ -51,8 +51,8 @@ Node newNode(ListElement data) {
 // Frees heap memory pointed to by *pN, sets *pN to NULL.
 void freeNode(Node* pN) {
 	if (pN != NULL && *pN != NULL) {
-	free(*pN);
-	*pN = NULL;
+		free(*pN);
+		*pN = NULL;
 	}
 }
 
@@ -64,16 +64,53 @@ List newList(void) {
 	L->front = NULL;
 	L->back = NULL;
 	L->cursor = NULL;
-	cursorIndex = -1;
-	int length = 0;
+	L->cursorIndex = -1;
+	L->length = 0;
+	return(L);
 }
 
 // freeList()
 // Frees all heap memory associated with *pL, and sets *pL to NULL
 void freeList(List* pL) {
 	if (pL != NULL && *pL != NULL) {
-		//free all nodes somehow
+		while((*pL)->front != NULL) {
+			Node currNode = (*pL)->front;
+			(*pL)->front = (*pL)->front->next;
+			freeNode(&currNode);
+		}
 	}
 	free(pL);
 	*pL = NULL;
+}
+
+// Access functions ------------------------------------------------------------
+
+// length() 
+// Returns the number of elements in L.
+int length(List L) {
+	int len = L->length;
+	return(len);
+}
+
+// index()
+// Returns index of cursor element if definded, -1 otherwise.
+int index(List L) {
+	int index = L->cursorIndex;
+	return(index);
+}
+
+// front()
+// Returns front element of L. Pre: length()>0
+int front(List L) {
+	assert(length(L) > 0);
+	int front = L->front->data;
+	return(front);
+}
+
+// back()
+// Returns back element of L. Pre: length>0
+int back(List L) {
+	assert(length(L) > 0);
+	int back = L->back->data;
+	return(back);
 }
---
+++ pa1/ListTest.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "List.h"
+
+int main(void) {
+	return(0);
+}
