f46a4892d8d7a7f1299d089eaf8c83c5f871d632
Sebastian Avila <snavila@ucsc.edu>
2024-02-16 22:08:58 -0800

    passes tests

--- pa4/Matrix.c
+++ pa4/Matrix.c
@@ -28,7 +28,7 @@ Matrix newMatrix(int n) {
 	M->size = n;
 	M->NNZ = 0;
 	for (int i = 1; i <= n; i++) {
-		M->row[i] = NULL;
+		M->row[i] = newList();
 	}
 	return(M);
 }
@@ -42,14 +42,8 @@ void freeEntry(Entry *pE) {
 
 void freeMatrix(Matrix *pM) {
 	if (pM != NULL && *pM != NULL) {
+		makeZero(*pM);
 		for (int i = 1; i <= (*pM)->size; i++) {
-			if ((*pM)->row[i] == NULL) {
-				continue;
-			}
-			for (moveFront((*pM)->row[i]); index((*pM)->row[i]) >= 0; moveNext((*pM)->row[i])) {
-				Entry E = (Entry) get((*pM)->row[i]);
-				freeEntry(&E);
-			}
 			freeList(&(*pM)->row[i]);
 		}
 		free((*pM)->row);
@@ -76,28 +70,21 @@ int NNZ(Matrix M) {
 }
 
 int equals(Matrix A, Matrix B) {
-	if (A->size != B->size || A->NNZ != B->NNZ) {
+	if (A->size != B->size) {
 		return(0);
 	}
+	Entry aE;
+	Entry bE;
 	for (int i = 1; i <= A->size; i++) {
-		if (A->row[i] == NULL && B->row[i] == NULL) {
-			continue;
-		}
-		if (A->row[i] == NULL || B->row[i] == NULL) {
+		if (length(A->row[i]) != length(B->row[i])) {
 			return(0);
 		}
 		moveFront(A->row[i]);
 		moveFront(B->row[i]);
-		while (1) {
-			if (index(A->row[i]) < 0 && index(B->row[i]) < 0) {
-				break;
-			}
-			if (index(A->row[i]) < 0 || index(B->row[i]) < 0) {			
-				return(0);
-			}
-			Entry aE = get(A->row[i]);
-			Entry bE = get(B->row[i]);
-			if (aE->val != bE->val) {
+		while(index(A->row[i]) >= 0) {
+			aE = (Entry) get(A->row[i]);
+			bE = (Entry) get(B->row[i]);
+			if (bE->col != aE->col || bE->val != aE->val) {
 				return(0);
 			}
 			moveNext(A->row[i]);
@@ -110,80 +97,72 @@ int equals(Matrix A, Matrix B) {
 
 // Manipulation procedures
 void makeZero(Matrix M) {
+	Entry E;
 	for (int i = 1; i <= M->size; i++) {
-		if (M->row[i] == NULL) {
-			continue;
-		}
 		for (moveFront(M->row[i]); index(M->row[i]) >= 0; moveNext(M->row[i])) {
-			Entry E = get(M->row[i]);
+			E = (Entry) get(M->row[i]);
 			freeEntry(&E);
 		}
-		freeList(&(M->row[i]));
-		M->row[i] = NULL;
+		clear(M->row[i]);
 	}
 	M->NNZ = 0;
 }
 
 void changeEntry(Matrix M, int i, int j, double x) {
 	if (1 > i || i > M->size || 1 > j || j > M->size) {
-		fprintf(stderr, "Matrix.c: error calling changeEntry() with invalid coordinates: \
-			 (%d,%d)\n", i, j);
+		fprintf(stderr, "Matrix.c: error calling changeEntry() with invalid coordinates: (%d,%d)\n", i, j);
 		exit(EXIT_FAILURE);
 	}
-	if (M->row[i] == NULL) {
-		M->row[i] = newList();
-	}
-	for (moveFront(M->row[i]); index(M->row[i]); moveNext(M->row[i])) {
-		if (length(M->row[i]) == 0) {
-			if (x != 0) {
-				Entry nE = newEntry(j, x);
-				append(M->row[i], nE);
-				M->NNZ++;
+	Entry E;
+	Entry nE;
+	if (x == 0) {
+		for (moveFront(M->row[i]); index(M->row[i]) >= 0; moveNext(M->row[i])) {
+			E = (Entry) get(M->row[i]);
+			if (E->col > j) {
 				return;
 			}
-			break;
-		}
-		Entry E = get(M->row[i]);
-		if (E->col == j) {
-			if (x == 0) {
-				freeEntry(get(M->row[i]));
+			if (E->col == j) {
+				freeEntry(&E);
 				delete(M->row[i]);
 				M->NNZ--;
 				return;
 			}
-			E->val = x;
-			return;
 		}
-		if (E->col > j) {
-			if (x == 0) {
-				return;
-			}
-			Entry N = newEntry(j, x);
-			insertBefore(M->row[i], N);
-			M->NNZ++;
-			return;
-		}
+		return;
 	}
-	if (x != 0) {
-		Entry E = newEntry(j, x);
-		append(M->row[i], E);
-		M->NNZ++;
-	}
+
 	if (length(M->row[i]) == 0) {
-		freeList(&M->row[i]);
-	}
-}
+		nE = newEntry(j, x);
+		append(M->row[i], nE);
+		M->NNZ++;
+		return;
+ 	}
+ 	for (moveFront(M->row[i]); index(M->row[i]) >= 0; moveNext(M->row[i])) {
+ 		E = (Entry) get(M->row[i]);
+ 		if (E->col == j) {
+ 			E->val = x;
+ 			return;
+ 		}
+ 		if (E->col > j) {
+ 			nE = newEntry(j, x);
+ 			insertBefore(M->row[i], nE);
+ 			M->NNZ++;
+ 			return;
+ 		}
+ 	}
+ 	nE = newEntry(j, x);
+ 	append(M->row[i], nE);
+ 	M->NNZ++;
+ 	return;
+ }
 
 
 // Matrix Arithmetic operations ------------------------------------------------
 Matrix copy(Matrix A) {
 	Matrix C = newMatrix(A->size);
 	for (int i = 1; i <= A->size; i++) {
-		if (A->row[i] == NULL) {
-			continue;
-		}
 		for (moveFront(A->row[i]); index(A->row[i]) >= 0; moveNext(A->row[i])) {
-			Entry E = get(A->row[i]);
+			Entry E = (Entry) get(A->row[i]);
 			changeEntry(C, i, E->col, E->val);
 		}
 	}
@@ -193,11 +172,8 @@ Matrix copy(Matrix A) {
 Matrix transpose(Matrix A) {
 	Matrix T = newMatrix(A->size);
 	for (int i = 1; i <= A->size; i++) {
-		if (A->row[i] == NULL) {
-			continue;
-		}
 		for (moveFront(A->row[i]); index(A->row[i]) >= 0; moveNext(A->row[i])) {
-			Entry E = get(A->row[i]);
+			Entry E = (Entry) get(A->row[i]);
 			changeEntry(T,E->col, i, E->val);
 		}
 	}			
@@ -207,149 +183,60 @@ Matrix transpose(Matrix A) {
 Matrix scalarMult(double x, Matrix A) {
 	Matrix xA = newMatrix(A->size);
 	if (x == 0) {
-		xA->NNZ = 0;
 		return(xA);
 	}
 	for (int i = 1; i <= A->size; i++) {
-		if (A->row[i] == NULL) {
-			continue;
-		}
 		for (moveFront(A->row[i]); index(A->row[i]) >= 0; moveNext(A->row[i])) {
-			Entry E = get(A->row[i]);
-			changeEntry(xA, i, E->col, x * E->val);
+			Entry E = (Entry) get(A->row[i]);
+			changeEntry(xA, i, E->col, (x * E->val));
 		}
 	}
 	return(xA);
 }
 
-double vectorDot(List P, List C) {
-	int sum = 0;
-	Entry pE;
-	Entry cE;
-	if (P == NULL || C == NULL) {	
-		return(0);
-	}
-	moveFront(P);
-	moveFront(C);
-	while (index(P) >= 0 && index(C) >= 0) {
-		pE = get(P);
-		cE = get(C);
-		if (pE->col < cE->col) {
-			moveNext(P);
-		} else if (pE->col > cE->col) {
-			moveNext(C);
-		} else {
-			sum += pE->val * cE->val;
-			moveNext(P);
-			moveNext(C);
-		}
-	}
-	return(sum);
-}
-
 List vectorSum(List P, List C) {
 	Entry pE;
 	Entry cE;
-	if (P == NULL && C == NULL) {	
-		return(NULL);
-	}
-	if (P == NULL) {
-		return(C);
-	}
-	if (C == NULL) {
-		return(P);
-	}
+	Entry E;
 	List sum = newList();
 	moveFront(P);
 	moveFront(C);
 	while (index(P) >= 0 && index(C) >= 0) {
-		pE = get(P);
-		cE = get(C);
+		pE = (Entry) get(P);
+		cE = (Entry) get(C);
 		if (pE->col < cE->col) {
-			Entry E = newEntry(pE->col, pE->val);
+			E = newEntry(pE->col, pE->val);
 			append(sum, E);
 			moveNext(P);
 		} else if (pE->col > cE->col) {
-			Entry E = newEntry(cE->col, cE->val);
+			E = newEntry(cE->col, cE->val);
 			append(sum, E);
 			moveNext(C);
 		} else {
-			Entry sE = newEntry(pE->col, pE->val + cE->val);
-			append(sum, sE);
-			moveNext(P);
-			moveNext(C);
-		}
-	}
-	 while (index(P) >= 0) {
-		Entry E = newEntry(pE->col, pE->val);
-		append(sum, E);
-                moveNext(P);
-        }
-        while (index(C) >= 0) {
-		Entry E = newEntry(cE->col, cE->val);
-		append(sum, E);
-                moveNext(C);
-        }
-	return(sum);
-}
-
-List vectorDiff(List P, List C) {
-	Entry cE;
-	if (P == NULL && C == NULL) {	
-		return(NULL);
-	}
-	if (C == NULL) {
-		return(P);
-	}
-	List diff = newList();
-	if (P == NULL) {
-		for (moveFront(C); index(C) >= 0; moveNext(C)) {
-			cE = get(C);
-			Entry dE = newEntry(cE->col, 0 - cE->val);
-			append(diff, dE);
-		}
-		return(diff);
-	}
-	moveFront(P);
-	moveFront(C);
-	while (index(P) >= 0 && index(C) >= 0) {
-		Entry pE = get(P);
-		Entry cE = get(C);
-		if (pE->col < cE->col) {
-			Entry E = newEntry(pE->col, pE->val);
-			append(diff, E);
-			moveNext(P);
-		} else if (pE->col > cE->col) {
-			Entry E = newEntry(cE->col, 0 - cE->val);
-			append(diff, E);
-			moveNext(C);
-		} else {
-			if (pE->val - cE->val != 0) {
-				Entry dE = newEntry(pE->col, pE->val - cE->val);
-				append(diff, dE);
+			if (pE->val + cE->val == 0) {
+				moveNext(P);
+				moveNext(C);
+				continue;
 			}
+			E = newEntry(pE->col, pE->val + cE->val);
+			append(sum, E);
 			moveNext(P);
 			moveNext(C);
-			
 		}
 	}
 	while (index(P) >= 0) {
-		Entry pE = get(P);
-		Entry E = newEntry(pE->col, pE->val);
-		append(diff, E);
+		pE = get(P);
+		E = newEntry(pE->col, pE->val);
+		append(sum, E);
 		moveNext(P);
 	}
 	while (index(C) >= 0) {
 		cE = get(C);
-		Entry dE = newEntry(cE->col, 0 - cE->val);
-		append(diff, dE);
+		E = newEntry(cE->col, cE->val);
+		append(sum, E);
 		moveNext(C);
 	}
-	if (length(diff) == 0) {
-		freeList(&diff);
-		return(NULL);
-	}
-	return(diff);
+	return(sum);
 }
 
 Matrix sum(Matrix A, Matrix B) {
@@ -357,12 +244,60 @@ Matrix sum(Matrix A, Matrix B) {
 		fprintf(stderr, "Matrix.c: error calling sum() with differently sized matrices\n");
 		exit(EXIT_FAILURE);
 	}
+	if (equals(A, B)) {
+		Matrix S = scalarMult(2, A);
+		return(S);
+	}
 	Matrix S = newMatrix(A->size);
 	S->NNZ = A->NNZ > B->NNZ ? A->NNZ : B->NNZ;
 	for (int i = 1; i <= S->size; i++) {
+		freeList(&S->row[i]);
 		S->row[i] = vectorSum(A->row[i], B->row[i]);
 	}
 	return(S);
+}
+
+List vectorDiff(List P, List C) {
+	Entry cE;
+	Entry pE;
+	Entry E;
+	List diff = newList();
+	moveFront(P);
+	moveFront(C);
+	while (index(P) >= 0 && index(C) >= 0) {
+		pE = get(P);
+		cE = get(C);
+		if (pE->col < cE->col) {
+			E = newEntry(pE->col, pE->val);
+			append(diff, E);
+			moveNext(P);
+		} else if (pE->col > cE->col) {
+			E = newEntry(cE->col, 0 - cE->val);
+			append(diff, E);
+			moveNext(C);
+		} else {
+			if (pE->val - cE->val != 0) {
+				E = newEntry(pE->col, pE->val - cE->val);
+				append(diff, E);
+			}
+			moveNext(P);
+			moveNext(C);
+			
+		}
+	}
+	while (index(P) >= 0) {
+		pE = get(P);
+		E = newEntry(pE->col, pE->val);
+		append(diff, E);
+		moveNext(P);
+	}
+	while (index(C) >= 0) {
+		cE = get(C);
+		E = newEntry(cE->col, 0 - cE->val);
+		append(diff, E);
+		moveNext(C);
+	}
+	return(diff);
 }
 
 Matrix diff(Matrix A, Matrix B) {
@@ -371,10 +306,42 @@ Matrix diff(Matrix A, Matrix B) {
 		exit(EXIT_FAILURE);
 	}
 	Matrix D = newMatrix(A->size);
-	for (int i = 1; i <= D->size; i++) {
-		D->row[i] = vectorDiff(A->row[i], B->row[i]);
+	for (int i = 1; i <= A->size; i++) {
+		freeList(&D->row[i]);
+		List row = vectorDiff(A->row[i], B->row[i]);
+		D->row[i] = row;
+		D->NNZ += length(row);
 	}
+	//Matrix nB = scalarMult(-1,B);
+	//Matrix D = sum(A, nB);
+	//freeMatrix(&nB);
 	return(D);
+}
+
+double vectorDot(List P, List C) {
+	double sum = 0;
+	Entry pE;
+	Entry cE;
+	if (length(P) == 0 || length(C) == 0) {	
+		return(0);
+	}
+	moveFront(P);
+	moveFront(C);
+	while (index(P) >= 0 && index(C) >= 0) {
+		pE = (Entry) get(P);
+		cE = (Entry) get(C);
+		if (pE->col < cE->col) {
+			moveNext(P);
+		} else if (pE->col > cE->col) {
+			moveNext(C);
+		} else {
+			double val = pE->val * cE->val;
+			sum += val;
+			moveNext(P);
+			moveNext(C);
+		}
+	}
+	return(sum);
 }
 
 Matrix product(Matrix A, Matrix B) {
@@ -385,22 +352,11 @@ Matrix product(Matrix A, Matrix B) {
 	Matrix P = newMatrix(A->size);
 	Matrix Bt = transpose(B);
 	for (int i = 1; i <= P->size; i++) {
-		if (A->row[i] == NULL) {
-			continue;
-		}
-		P->row[i] = newList();
 		for (int j = 1; j <= P->size; j++) {
-			if (Bt->row[i] == NULL) {
-				continue;
+			double val = vectorDot(A->row[i], Bt->row[j]);
+			if (val != 0) {
+				changeEntry(P,i, j, val);
 			}
-			int val = vectorDot(A->row[i], Bt->row[j]);
-			if (val == 0) {
-				continue;
-			}
-			changeEntry(P,i, j, val);
-		}
-		if (length(P->row[i]) == 0) {
-			freeList(&P->row[i]);
 		}
 	}
 	freeMatrix(&Bt);
@@ -411,12 +367,12 @@ Matrix product(Matrix A, Matrix B) {
 // Other operations ------------------------------------------------------------
 void printMatrix(FILE* out, Matrix M) {
 	for (int i = 1; i <= M->size; i++) {
-		if (M->row[i] == NULL) {
+		if (length(M->row[i]) == 0) {
 			continue;
 		}
 		fprintf(out, "%d:", i);
 		for (moveFront(M->row[i]); index(M->row[i]) >= 0; moveNext(M->row[i])) {
-			Entry E = get(M->row[i]);
+			Entry E = (Entry) get(M->row[i]);
 			fprintf(out, "(%d, %.1f) ", E->col,  E->val);
 		}
 		fprintf(out, "\n");
